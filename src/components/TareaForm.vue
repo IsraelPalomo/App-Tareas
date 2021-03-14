0634cbe1cdf7c4ecf3390339909553705e4f2652
IsraelPalomo <78044199+IsraelPalomo@users.noreply.github.com>
2021-03-14 11:29:55 +0100

    APP Terminada

---
+++ src/components/TareaForm.vue
@@ -0,0 +1,37 @@
+<template>
+	<form @submit.prevent="agregarTarea">
+		<input
+			type="text"
+			placeholder="Ingrese Tarea"
+			class="form-control my-2 text-center"
+			v-model.trim="texto"
+		/>
+		<button class="btn btn-primary w-100">Agregar</button>
+	</form>
+</template>
+
+<script>
+import { inject, ref } from "vue";
+export default {
+	setup() {
+		const texto = ref("");
+		const tareas = inject("tareas");
+		const agregarTarea = () => {
+			if (texto.value === "") {
+				return console.log("Texto Vacio");
+			}
+			const tarea = {
+				texto: texto.value,
+				estado: false,
+				id: Date.now(),
+			};
+
+			tareas.value.push(tarea);
+			texto.value = "";
+		};
+		return { agregarTarea, texto };
+	},
+};
+</script>
+
+<style></style>
